0d234b57c27e51e688c887b962fada0f0934bf86
TNXG <tnxg@outlook.jp>
2024-07-14 23:08:13 +0800

    fix: Improve the image acquisition method of DOMProvider

--- src/SongInfoProviders/DOMProvider.ts
+++ src/SongInfoProviders/DOMProvider.ts
@@ -35,13 +35,19 @@ export class DOMProvider extends BaseProvider {
         }, 50);
 
         setInterval(() => {
-            if (document.querySelector(".j-cover, .cmd-image")) {
-                const imgUrl = (
-                    document?.querySelector(".j-cover, .cmd-image") as HTMLImageElement
-                )?.src
-                    .replace("orpheus://cache/?", "")
-                    .replace(/\?(.*)/, "");
-
+            let imgUrl
+            const miniVinylWrappers = document.querySelectorAll('.miniVinylWrapper');
+            miniVinylWrappers.forEach(wrapper => {
+                const imgElement = wrapper.querySelector('img');
+                if (imgElement) {
+                    imgUrl = imgElement.src;
+                    const url = new URL(imgUrl);
+                    url.search = '';
+                    imgUrl = url.toString();
+                }
+            });
+            if (imgUrl) {
+                console.log('imgurl yes')
                 if (lastImgUrl === imgUrl) return;
                 lastImgUrl = imgUrl;
                 console.log("UPDATE");
@@ -54,18 +60,20 @@ export class DOMProvider extends BaseProvider {
                         ".cmd-space.title > span",
                     ) as HTMLParagraphElement | null
                 )?.innerText;
+                const data = {
+                    songName,
+                    albumName: songName,
+                    authorName: (
+                        document.querySelector(
+                            "p.j-title span.f-dib, .cmd-space.title .author",
+                        ) as HTMLParagraphElement
+                    ).innerText,
+                    thumbnail: imgUrl,
+                }
+                console.log(data)
                 this.dispatchEvent(
                     new CustomEvent("updateSongInfo", {
-                        detail: {
-                            songName,
-                            albumName: songName,
-                            authorName: (
-                                document.querySelector(
-                                    "p.j-title span.f-dib, .cmd-space.title .author",
-                                ) as HTMLParagraphElement
-                            ).innerText,
-                            thumbnail: imgUrl,
-                        },
+                        detail: data,
                     }),
                 );
             }
