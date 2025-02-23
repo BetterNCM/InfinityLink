4030074513c4e1f789ba31d78a110b212c2fab6a
LgCookie <lgc2333@126.com>
2025-02-23 22:49:43 +0800

    add frontend media session smtc impl & fix ncm2 frontend info provider timeline

--- src/index.tsx
+++ src/index.tsx
@@ -15,19 +15,37 @@ import {
 } from "react-icons/md";
 import { useLocalStorage } from "./hooks";
 import "./index.scss";
-import { Alert, Card, Divider, FormControlLabel, FormGroup, FormLabel, Radio, RadioGroup, Switch, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import {
+    Alert,
+    Card,
+    Divider,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    Radio,
+    RadioGroup,
+    Switch,
+    TextField,
+    ToggleButton,
+    ToggleButtonGroup,
+    Typography,
+} from "@mui/material";
 import { render } from "react-dom";
 import * as React from "react";
 import Button from "@mui/material/Button";
-import { STORE_KEY_DCRPC_ENABLED, STORE_KEY_INFO_PROVIDER, STORE_KEY_SMTC_ENABLED } from "./keys";
+import {
+    STORE_KEY_DCRPC_ENABLED,
+    STORE_KEY_INFO_PROVIDER,
+    STORE_KEY_SMTC_ENABLED,
+} from "./keys";
 import { DOMProvider } from "./SongInfoProviders/DOMProvider";
 import { BaseProvider } from "./SongInfoProviders/BaseProvider";
 import { NativeProvider } from "./SongInfoProviders/NativeProvider";
 
-
 // receivers
 import { SMTC } from "./Receivers/smtc";
 import { DCRPC } from "./Receivers/dc-rpc";
+import { SMTCFrontend } from "./Receivers/smtc-frontend";
 
 let configElement = document.createElement("div");
 
@@ -38,10 +56,7 @@ plugin.onLoad((selfPlugin) => {
 function Main() {
     const nativePluginLoaded = React.useMemo(() => {
         try {
-            return (
-                betterncm_native.native_plugin.call("inflink.test", []) ===
-                "true"
-            );
+            return betterncm_native.native_plugin.call("inflink.test", []) === "true";
         } catch (e) {
             return false;
         }
@@ -55,33 +70,36 @@ function Main() {
                         Native Plugin未加载！
                     </Typography>
                     <Typography fontSize="sm" sx={{ opacity: 0.8 }}>
-                        InfLink将无法使用，请确定您使用的是 1.0.0-pre2
-                        及以上版本的 BetterNCM。
-                        若不是 请通过 BetterNCM Installer 更新。
+                        InfLink将无法使用，请确定您使用的是 1.0.0-pre2 及以上版本的
+                        BetterNCM。 若不是 请通过 BetterNCM Installer 更新。
                     </Typography>
                 </Alert>
             </div>
         );
     }
 
-    const [SMTCEnabled, setSMTCEnabled] = useLocalStorage(
-        STORE_KEY_SMTC_ENABLED,
-        true,
-    );
+    const [SMTCEnabled, setSMTCEnabled] = useLocalStorage(STORE_KEY_SMTC_ENABLED, true);
 
     const [DCRPCEnabled, setDCRPCEnabled] = useLocalStorage(
         STORE_KEY_DCRPC_ENABLED,
         false,
-    )
-
+    );
 
     const [infoProviderName, setInfoProviderName] = useLocalStorage(
         STORE_KEY_INFO_PROVIDER,
         "dom",
     );
 
-    const [InfoProvider, setInfoProvider] = React.useState<BaseProvider | null>(
-        null,
+    const [InfoProvider, setInfoProvider] = React.useState<BaseProvider | null>(null);
+
+    const [smtcImpl, setSmtcImpl] = useLocalStorage<"native" | "frontend">(
+        STORE_KEY_SMTC_IMPL,
+        "native",
+    );
+
+    const smtcImplObj = React.useMemo(
+        () => (smtcImpl === "native" ? SMTC : SMTCFrontend),
+        [smtcImpl],
     );
 
     React.useEffect(() => {
@@ -98,49 +116,43 @@ function Main() {
         InfoProvider?.dispatchEvent(new CustomEvent("enable"));
     }, [InfoProvider]);
 
-
     React.useEffect(() => {
         async function onUpdateSongInfo(e) {
-            if (SMTCEnabled)
-                SMTC.update(e.detail);
+            if (SMTCEnabled) smtcImplObj.update(e.detail);
 
-            if (DCRPCEnabled)
-                DCRPC.update(e.detail);
+            if (DCRPCEnabled) DCRPC.update(e.detail);
         }
 
         function onUpdatePlayState(e) {
             const state = e.detail === "Playing" ? 3 : 4;
 
-            if (SMTCEnabled)
-                SMTC.updatePlayState(state);
+            if (SMTCEnabled) smtcImplObj.updatePlayState(state);
         }
 
         async function onUpdateTimeline(e) {
-            if (SMTCEnabled)
-                SMTC.updateTimeline(e.detail);
+            if (SMTCEnabled) smtcImplObj.updateTimeline(e.detail);
         }
 
-        InfoProvider?.addEventListener(
-            "updatePlayState",
-            onUpdatePlayState,
-        );
-
+        InfoProvider?.addEventListener("updatePlayState", onUpdatePlayState);
         InfoProvider?.addEventListener("updateSongInfo", onUpdateSongInfo);
-
         InfoProvider?.addEventListener("updateTimeline", onUpdateTimeline);
-    }, [InfoProvider]);
+
+        return () => {
+            InfoProvider?.removeEventListener("updatePlayState", onUpdatePlayState);
+            InfoProvider?.removeEventListener("updateSongInfo", onUpdateSongInfo);
+            InfoProvider?.removeEventListener("updateTimeline", onUpdateTimeline);
+        };
+    }, [InfoProvider, smtcImplObj]);
 
     React.useEffect(() => {
+        SMTC.disable();
+        SMTCFrontend.disable();
         if (SMTCEnabled) {
-            SMTC.apply(msg => {
-                InfoProvider?.dispatchEvent(
-                    new CustomEvent(msg),
-                );
-            })
-        } else {
-            SMTC.disable();
+            smtcImplObj.apply((msg) => {
+                InfoProvider?.dispatchEvent(new CustomEvent(msg));
+            });
         }
-    }, [InfoProvider, SMTCEnabled]);
+    }, [InfoProvider, SMTCEnabled, smtcImplObj]);
 
     React.useEffect(() => {
         if (DCRPCEnabled) {
@@ -159,20 +171,59 @@ function Main() {
                     name="infoprovider"
                 >
                     <FormControlLabel value="dom" control={<Radio />} label="DOM" />
-                    <FormControlLabel value="native" control={<Radio />} label="原生 (3.0.0 不可用)" />
+                    <FormControlLabel
+                        value="native"
+                        control={<Radio />}
+                        label="原生 (3.0.0 不可用)"
+                    />
                 </RadioGroup>
 
                 <div>
-                    <FormControlLabel control={<Switch
-                        checked={SMTCEnabled}
-                        onChange={(e, checked) => setSMTCEnabled(checked)}
-                    />} label="开启 SMTC" />
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={SMTCEnabled}
+                                onChange={(e, checked) => setSMTCEnabled(checked)}
+                            />
+                        }
+                        label="开启 SMTC"
+                    />
 
-                    <FormControlLabel control={<Switch
-                        checked={DCRPCEnabled}
-                        onChange={(e, checked) => setDCRPCEnabled(checked)}
-                    />} label="开启 Discord RPC" />
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={DCRPCEnabled}
+                                onChange={(e, checked) => setDCRPCEnabled(checked)}
+                            />
+                        }
+                        label="开启 Discord RPC"
+                    />
                 </div>
+
+                {SMTCEnabled ? (
+                    <div>
+                        <FormLabel>SMTC 实现</FormLabel>
+                        <RadioGroup
+                            row
+                            defaultValue={smtcImpl}
+                            onChange={(_, v) => setSmtcImpl(v as any)}
+                            name="smtcImpl"
+                        >
+                            <FormControlLabel
+                                value="native"
+                                control={<Radio />}
+                                label="原生"
+                            />
+                            <FormControlLabel
+                                value="frontend"
+                                control={<Radio />}
+                                label="前端（可覆盖掉 LibFrontendPlay 的 SMTC 信息）"
+                            />
+                        </RadioGroup>
+                    </div>
+                ) : (
+                    <></>
+                )}
             </FormGroup>
         </div>
     );
