f5910f82481cbc0239a041f7b6d702b292a50dfc
LgCookie <lgc2333@126.com>
2025-04-06 17:49:54 +0800

    add throttle

--- src/utils.ts
+++ src/utils.ts
@@ -35,3 +35,37 @@ export function createHookFn(
 }
 
 export const reactInstance = (e: HTMLElement) => e[Object.keys(e).find(v => v.includes('__reactInternalInstance'))!]
+
+// from https://github.com/martinstark/throttle-ts
+export const throttle = <R, A extends any[]>(
+    fn: (...args: A) => R,
+    delay: number,
+): [(...args: A) => R | undefined, () => void, () => void] => {
+    let wait = false;
+    let timeout: undefined | number;
+    let cancelled = false;
+
+    function resetWait() {
+        wait = false;
+    }
+
+    return [
+        (...args: A) => {
+            if (cancelled) return undefined;
+            if (wait) return undefined;
+            const val = fn(...args);
+            wait = true;
+            timeout = window.setTimeout(resetWait, delay);
+            return val;
+        },
+        () => {
+            cancelled = true;
+            clearTimeout(timeout);
+        },
+        () => {
+            clearTimeout(timeout);
+            resetWait();
+        },
+    ];
+};
+
